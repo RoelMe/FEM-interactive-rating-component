3b833f21eca596b6bdec3ddef43321eb143fba3b
RuleMe <roel.meeusen@gmail.com>
2022-09-16 09:37:05 +0200

    updated functionality of selectors and added thank-you page for form submission

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
-let rating = ''
-const yourRating = document.querySelector('.your-rating')
-const form = document.querySelector('form')
-const choices = document.querySelectorAll('.rating__number')
-const survey = document.querySelector('.card')
-const thankYou = document.querySelector('.center')
-for (let choice of choices) {
-    console.log(choice.value)
-    choice.addEventListener('click', () => {
-        rating = choice.value
-    })
-}
-
-form.addEventListener('submit', () => {
-    yourRating.innerText = rating
-    survey.classList.add('hidden')
-    thankYou.classList.remove('hidden')
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const ratingOptions = document.querySelectorAll('input[name="rating"]')
+    let yourRating
+            for (const ratingOption of ratingOptions) {
+                if (ratingOption.checked) {
+                    yourRating = ratingOption.value;
+                    break;
+                }
+            }
+    
+        form.submit()
 })
